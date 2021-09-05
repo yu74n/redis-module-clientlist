9139a67baa4b2d7be38fc0ca9c9b07e1c82d5963
Yuta Hongo <yutago@gmail.com>
2021-09-05 18:24:39 +0900

    Store client list in Sets

--- src/clientlist.c
+++ src/clientlist.c
@@ -1,10 +1,14 @@
 #include "redismodule.h"
 
+static const char* CLIENT_KEY_NAME = "clientlist";
+
 void ClientList_EventCallBack(RedisModuleCtx *ctx, RedisModuleEvent eid, uint64_t subevent, void *data) {
     RedisModuleClientInfo *client_info = (RedisModuleClientInfo *) data;
 
     if (subevent == REDISMODULE_SUBEVENT_CLIENT_CHANGE_CONNECTED) {
         RedisModule_Log(ctx, REDISMODULE_LOGLEVEL_NOTICE, "Connected: %s:%d (clientId=%lld, flags=%lld)\n", client_info->addr, client_info->port, client_info->id, client_info->flags);
+        RedisModuleCallReply* reply = RedisModule_Call(ctx, "SADD", "cc", CLIENT_KEY_NAME, client_info->addr);
+        RedisModule_FreeCallReply(reply);
     } else if (subevent == REDISMODULE_SUBEVENT_CLIENT_CHANGE_DISCONNECTED) {
         RedisModule_Log(ctx, REDISMODULE_LOGLEVEL_NOTICE, "Disconnected: %s:%d(clientId=%lld, flags=%lld)\n", client_info->addr, client_info->port, client_info->id, client_info->flags);
     }
